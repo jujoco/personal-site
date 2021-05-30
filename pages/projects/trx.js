fcee4abb856d7ba41eca194c54d48451847eb1a7
jujoco <jujocotm@gmail.com>
2021-05-29 20:07:55 -0700

    next.js init

---
+++ pages/projects/trx.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+export default function TRX() {
+  return (
+    <>
+      <h1>TRX Project</h1>
+      <Link href="/">
+        <a>back</a>
+      </Link>
+    </>
+  )
+}
